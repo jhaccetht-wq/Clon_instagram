85daeabdd364e3ab4744444f7e9f662c57ff0ee7
Adrian <jhaccetht@gmail.com>
2026-02-04 07:20:31 -0500

    resolviendo problema de ruta principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import AppRouter from './router/Approuter'
+import AppRouter from './router/AppRouter'
 import "../src/style/tailwind.css"
 
 
